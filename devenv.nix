6bcef1010d681c10225521c30c7b82a0602fa036
drangon <code@kmr.pw>
2025-10-22 21:44:47 -0400

    add secretspec

--- devenv.nix
+++ devenv.nix
@@ -27,4 +27,13 @@
     mix-format.enable = true;
     mixed-line-endings.enable = true;
   };
+
+  env = {
+    SONNET_OIDCC_CLIENT_ID = config.secretspec.secrets.SONNET_OIDCC_CLIENT_ID;
+    SONNET_OIDCC_CLIENT_SECRET = config.secretspec.secrets.SONNET_OIDCC_CLIENT_SECRET;
+    SONNET_OIDCC_ISSUER = config.secretspec.secrets.SONNET_OIDCC_ISSUER;
+    SONNET_S3_ACCESS_KEY_ID = config.secretspec.secrets.SONNET_S3_ACCESS_KEY_ID;
+    SONNET_S3_ACCESS_KEY_SECRET = config.secretspec.secrets.SONNET_S3_ACCESS_KEY_SECRET;
+    SONNET_S3_HOST = config.secretspec.secrets.SONNET_S3_HOST;
+  };
 }
