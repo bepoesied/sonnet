11749c29e7a7119d35c0ebd4886efc57fb568c78
drangon <code@kmr.pw>
2025-12-04 22:30:17 -0500

    remove dead tests
    fix up validation on changeset

--- lib/sonnet/library/chapter.ex
+++ lib/sonnet/library/chapter.ex
@@ -15,9 +15,32 @@ defmodule Sonnet.Library.Chapter do
   end
 
   @doc false
-  def changeset(chapter, attrs) do
+  def changeset(chapter, attrs \\ %{}) do
     chapter
-    |> cast(attrs, [:title, :start_ms, :end_ms, :position, :book_idk, :media_asset_id])
+    |> cast(attrs, [:title, :start_ms, :end_ms, :position, :book_id, :media_asset_id])
     |> validate_required([:title, :start_ms, :end_ms, :position, :book_id, :media_asset_id])
+    |> validate_number(:start_ms, greater_than: 0)
+    |> validate_number(:end_ms, greater_than: 0)
+    |> validate_greater_than(:end_ms, :start_ms)
+  end
+
+  defp validate_greater_than(changeset, field, other_field) do
+    value = get_field(changeset, field)
+    other_value = get_field(changeset, other_field)
+
+    cond do
+      is_nil(value) or is_nil(other_value) ->
+        changeset
+
+      value > other_value ->
+        changeset
+
+      true ->
+        add_error(
+          changeset,
+          field,
+          "#{field} must be greater than #{other_field}"
+        )
+    end
   end
 end
