8d1213e483a5937084f9c18f23ddd5725458abb4
drangon <code@kmr.pw>
2025-12-19 00:43:37 -0500

    move books to their hash before ingesting for dedup

--- lib/sonnet/workers/ingester.ex
+++ lib/sonnet/workers/ingester.ex
@@ -4,37 +4,48 @@ defmodule Sonnet.Workers.Ingester do
   @impl Oban.Worker
   def perform(%Oban.Job{args: %{"s3_key" => s3_key} = _args}) do
     path = download_from_s3!(s3_key)
-    probe = probe_file!(path)
 
-    cover_s3_key =
-      if has_video_stream?(probe) do
-        case extract_cover(path) do
-          {:ok, cover_path} ->
-            hash = calculate_hash(cover_path)
-            upload_cover(cover_path, hash)
+    new_s3_key = calculate_hash(path)
 
-          :error ->
+    cond do
+      s3_object_exists?(new_s3_key) ->
+        delete_from_s3(s3_key)
+        :ok
+
+      true ->
+        rename_in_s3(s3_key, new_s3_key)
+
+        s3_key = new_s3_key
+        probe = probe_file!(path)
+
+        cover_s3_key =
+          if has_video_stream?(probe) do
+            case extract_cover(path) do
+              {:ok, cover_path} ->
+                hash = calculate_hash(cover_path)
+                upload_cover(cover_path, hash)
+
+              :error ->
+                nil
+            end
+          else
             nil
-        end
-      else
-        nil
-      end
+          end
 
-    media_asset = Sonnet.Library.create_media_asset!(s3_key)
+        media_asset = Sonnet.Library.create_media_asset!(s3_key)
 
-    Sonnet.Library.ingest_probe!(probe, media_asset.id, cover_s3_key)
+        Sonnet.Library.ingest_probe!(probe, media_asset.id, cover_s3_key)
 
-    :ok
+        :ok
+    end
   end
 
   defp download_from_s3!(s3_key) do
-    bucket = Application.get_env(:sonnet, :ingest_bucket)
-    prefix = Application.get_env(:sonnet, :ingest_prefix)
-    key = "#{prefix}/#{s3_key}"
+    key = full_key(s3_key)
 
     path = Briefly.create!(type: :path)
 
-    ExAws.S3.download_file(bucket, key, path)
+    ExAws.S3.download_file(bucket(), key, path)
     |> ExAws.request!()
 
     path
@@ -86,15 +97,53 @@ defmodule Sonnet.Workers.Ingester do
   end
 
   defp upload_cover(path, hash) do
-    bucket = Application.get_env(:sonnet, :ingest_bucket)
-    prefix = Application.get_env(:sonnet, :ingest_prefix)
-    key = "#{prefix}/covers/#{hash}.jpg"
+    key = "#{prefix()}/covers/#{hash}.jpg"
 
     path
     |> ExAws.S3.Upload.stream_file()
-    |> ExAws.S3.upload(bucket, key)
+    |> ExAws.S3.upload(bucket(), key)
     |> ExAws.request!()
 
     key
+  end
+
+  defp s3_object_exists?(s3_key) do
+    key = full_key(s3_key)
+
+    case ExAws.S3.head_object(bucket(), key) |> ExAws.request() do
+      {:ok, _} -> true
+      {:error, {:http_error, 404, _}} -> false
+      {:error, reason} -> raise "failed to check s3 object existence: #{inspect(reason)}"
+    end
+  end
+
+  defp rename_in_s3(old_s3_key, new_s3_key) do
+    old_key = full_key(old_s3_key)
+    new_key = full_key(new_s3_key)
+
+    ExAws.S3.put_object_copy(bucket(), new_key, bucket(), old_key)
+    |> ExAws.request!()
+
+    ExAws.S3.delete_object(bucket(), old_key)
+    |> ExAws.request!()
+  end
+
+  defp delete_from_s3(s3_key) do
+    key = full_key(s3_key)
+
+    ExAws.S3.delete_object(bucket(), key)
+    |> ExAws.request!()
+  end
+
+  defp bucket do
+    Application.get_env(:sonnet, :ingest_bucket)
+  end
+
+  defp prefix do
+    Application.get_env(:sonnet, :ingest_prefix)
+  end
+
+  defp full_key(s3_key) do
+    "#{prefix()}/#{s3_key}"
   end
 end
